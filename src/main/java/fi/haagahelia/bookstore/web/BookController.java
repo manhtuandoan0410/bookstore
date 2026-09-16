648ef04e1599d6ace5813cd84fa1c2b0d411ceb0
Tuan Doan <tuan.doan@myy.haaga-helia.fi>
2026-09-16 23:52:04 +0300

    button delete and Addbook

--- src/main/java/fi/haagahelia/bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -2,6 +2,8 @@ package fi.haagahelia.bookstore.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import fi.haagahelia.bookstore.domain.BookRepository;
@@ -22,6 +24,10 @@ public class BookController {
 		return "booklist";
 	}
 	
-	
+	@GetMapping("/book/delete/{id}")
+	public String deleteBook(@PathVariable("id") Long id) {
+    	bookRepository.deleteById(id);
+    	return "redirect:/booklist";
+	}
 
 }

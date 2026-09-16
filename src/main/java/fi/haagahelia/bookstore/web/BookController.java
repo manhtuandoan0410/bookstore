64b15646ba3ad6f0d17f371ec574520571a958f1
Tuan Doan <tuan.doan@myy.haaga-helia.fi>
2026-09-17 00:12:43 +0300

    Add bookstore application

--- src/main/java/fi/haagahelia/bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -3,31 +3,51 @@ package fi.haagahelia.bookstore.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import fi.haagahelia.bookstore.domain.Book;
 import fi.haagahelia.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
-    
 
-	private final BookRepository bookRepository;
+    private final BookRepository bookRepository;
 
-	public BookController(BookRepository bookRepository) {
-		this.bookRepository = bookRepository;
-	}
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
     @RequestMapping(value = {"/", "/booklist"})
-	public String listbooks(Model model) {
-		model.addAttribute("books", bookRepository.findAll());
-		return "booklist";
-	}
-	
-	@GetMapping("/book/delete/{id}")
-	public String deleteBook(@PathVariable("id") Long id) {
-    	bookRepository.deleteById(id);
-    	return "redirect:/booklist";
-	}
+    public String listBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "booklist";
+    }
 
+    @GetMapping("/book/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long id) {
+        bookRepository.deleteById(id);
+        return "redirect:/booklist";
+    }
+
+    @GetMapping("/book/edit/{id}")
+    public String editBook(@PathVariable("id") Long id, Model model) {
+        Book book = bookRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid book id: " + id));
+
+        model.addAttribute("book", book);
+        return "editbook";
+    }
+
+    @PostMapping("/book/edit/{id}")
+    public String saveEditedBook(
+            @PathVariable("id") Long id,
+            @ModelAttribute("book") Book book) {
+
+        book.setId(id);
+        bookRepository.save(book);
+        return "redirect:/booklist";
+    }
 }
